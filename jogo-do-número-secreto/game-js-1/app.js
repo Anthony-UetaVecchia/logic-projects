306c4d5dde8f888bb4234efa2d14b97b9e84dd73
Anthony Ueta Vecchia <tony.ueta@gmail.com>
2024-05-21 13:59:10 -0300

    Syncing projects to the repository

---
+++ jogo-do-número-secreto/game-js-1/app.js
@@ -0,0 +1,30 @@
+alert('Bem vindo ao jogo do número secreto');
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+// enquanto chute não for igual ao número secreto
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (numeroSecreto > chute) {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        if (numeroSecreto < chute) {
+            alert(`O número secreto é menor que ${chute}`);
+        }
+        tentativas++;
+    }
+}
+// if (tentativas > 1) {
+//     alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`)
+// } else {
+//     alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`)
+// }
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`)
