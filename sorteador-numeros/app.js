346f7fe868ffcb5551166791a4f5d002e0e319a0
Anthony Ueta Vecchia <tony.ueta@gmail.com>
2024-06-11 23:13:11 -0300

    Creating Drawn function

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -2,8 +2,17 @@ function sortear(){
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
+    let sorteados = [];
+    let numero
 
-    alert(`Quantidade: ${quantidade}`);
-    alert(`Do número: ${de}`);
-    alert(`Até o número: ${ate}`);
+    for(let i = 0; i < quantidade; i++){
+        numero = obter_numero_aleatorio(de, ate);
+        sorteados.push(numero);
+    }
+
+    alert(sorteados);
+}
+
+function obter_numero_aleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
