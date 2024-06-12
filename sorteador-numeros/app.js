f4c0a89d98dd44c75ba116fe86602aa5527008b0
Anthony Ueta Vecchia <tony.ueta@gmail.com>
2024-06-12 02:44:22 -0300

    Displaying the drawn numbers

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -2,15 +2,19 @@ function sortear(){
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
-    let sorteados = [];
-    let numero
 
+    let sorteados = [];
+    let numero;
     for(let i = 0; i < quantidade; i++){
         numero = obter_numero_aleatorio(de, ate);
+        while(sorteados.includes(numero)){
+            numero = obter_numero_aleatorio(de, ate);
+        }
         sorteados.push(numero);
     }
 
-    alert(sorteados);
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
 }
 
 function obter_numero_aleatorio(min, max){
