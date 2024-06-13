eb2087e1a62d53d8201ed6f7c0b4103a2ced28cb
Anthony Ueta Vecchia <tony.ueta@gmail.com>
2024-06-12 23:41:03 -0300

    Creating Add and Clean functions

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,38 @@
+let subtotal;
+limpar();
+
+function adicionar(){
+    let produto = document.getElementById('produto').value;
+    let quantidade = document.getElementById('quantidade').value;
+
+    if (!produto || produto.trim() === "") {
+        alert("Selecione um produto válido.");
+        return;
+    }
+
+    if (isNaN(quantidade) || quantidade <= 0) {
+        alert("Insira uma quantidade válida.");
+        return;
+    }
+
+    let nomeProduto = produto.split('-')[0];
+    let valorUnit = parseInt(produto.split('R$')[1]);
+    let preco = valorUnit * quantidade;
+
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+        <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+    </section>`;
+
+    subtotal = subtotal + preco;
+    let campoSubtotal = document.getElementById('valor-total');
+    campoSubtotal.textContent = `R$${subtotal}`;
+
+    document.getElementById('quantidade').value = '';
+}
+
+function limpar(){
+    subtotal = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$0'; 
+}
